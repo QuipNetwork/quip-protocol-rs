78c8312542c9f9bcf0a8d6560414bb3775303905
Roman Useinov <roman.useinov@gmail.com>
2026-04-10 06:59:33 +0000

    (P1 -> P3) --> main

---
+++ Makefile
@@ -0,0 +1,2 @@
+polkadot-sdk:
+	git clone --branch polkadot-stable2603 git@github.com:QuipNetwork/polkadot-sdk.git
--- node/src/rpc.rs
+++ node/src/rpc.rs
@@ -8,53 +8,81 @@
 use std::sync::Arc;
 
 use jsonrpsee::RpcModule;
+use quip_protocol_runtime::{opaque::Block, AccountId, Balance, Nonce};
+use sc_consensus_babe::BabeWorkerHandle;
 use sc_transaction_pool_api::TransactionPool;
-use solochain_template_runtime::{opaque::Block, AccountId, Balance, Nonce};
 use sp_api::ProvideRuntimeApi;
 use sp_block_builder::BlockBuilder;
 use sp_blockchain::{Error as BlockChainError, HeaderBackend, HeaderMetadata};
+use sp_consensus::SelectChain;
+use sp_keystore::KeystorePtr;
+
+/// Extra dependencies for BABE.
+pub struct BabeDeps {
+    /// A handle to the BABE worker for issuing requests.
+    pub babe_worker_handle: BabeWorkerHandle<Block>,
+    /// The keystore that manages the keys of the node.
+    pub keystore: KeystorePtr,
+}
 
 /// Full client dependencies.
-pub struct FullDeps<C, P> {
-	/// The client instance to use.
-	pub client: Arc<C>,
-	/// Transaction pool instance.
-	pub pool: Arc<P>,
+pub struct FullDeps<C, P, SC> {
+    /// The client instance to use.
+    pub client: Arc<C>,
+    /// Transaction pool instance.
+    pub pool: Arc<P>,
+    /// The SelectChain strategy.
+    pub select_chain: SC,
+    /// BABE specific dependencies.
+    pub babe: BabeDeps,
 }
 
 /// Instantiate all full RPC extensions.
-pub fn create_full<C, P>(
-	deps: FullDeps<C, P>,
+pub fn create_full<C, P, SC>(
+    deps: FullDeps<C, P, SC>,
 ) -> Result<RpcModule<()>, Box<dyn std::error::Error + Send + Sync>>
 where
-	C: ProvideRuntimeApi<Block>,
-	C: HeaderBackend<Block> + HeaderMetadata<Block, Error = BlockChainError> + 'static,
-	C: Send + Sync + 'static,
-	C::Api: substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Nonce>,
-	C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
-	C::Api: BlockBuilder<Block>,
-	P: TransactionPool + 'static,
+    C: ProvideRuntimeApi<Block>,
+    C: HeaderBackend<Block> + HeaderMetadata<Block, Error = BlockChainError> + 'static,
+    C: Send + Sync + 'static,
+    C::Api: substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Nonce>,
+    C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
+    C::Api: sp_consensus_babe::BabeApi<Block>,
+    C::Api: BlockBuilder<Block>,
+    P: TransactionPool + 'static,
+    SC: SelectChain<Block> + Clone + 'static,
 {
-	use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApiServer};
-	use substrate_frame_rpc_system::{System, SystemApiServer};
+    use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApiServer};
+    use sc_consensus_babe_rpc::{Babe, BabeApiServer};
+    use substrate_frame_rpc_system::{System, SystemApiServer};
 
-	let mut module = RpcModule::new(());
-	let FullDeps { client, pool } = deps;
+    let mut module = RpcModule::new(());
+    let FullDeps {
+        client,
+        pool,
+        select_chain,
+        babe,
+    } = deps;
+    let BabeDeps {
+        babe_worker_handle,
+        keystore,
+    } = babe;
 
-	module.merge(System::new(client.clone(), pool).into_rpc())?;
-	module.merge(TransactionPayment::new(client).into_rpc())?;
+    module.merge(System::new(client.clone(), pool).into_rpc())?;
+    module.merge(TransactionPayment::new(client.clone()).into_rpc())?;
+    module.merge(Babe::new(client, babe_worker_handle, keystore, select_chain).into_rpc())?;
 
-	// Extend this RPC with a custom API by using the following syntax.
-	// `YourRpcStruct` should have a reference to a client, which is needed
-	// to call into the runtime.
-	// `module.merge(YourRpcTrait::into_rpc(YourRpcStruct::new(ReferenceToClient, ...)))?;`
+    // Extend this RPC with a custom API by using the following syntax.
+    // `YourRpcStruct` should have a reference to a client, which is needed
+    // to call into the runtime.
+    // `module.merge(YourRpcTrait::into_rpc(YourRpcStruct::new(ReferenceToClient, ...)))?;`
 
-	// You probably want to enable the `rpc v2 chainSpec` API as well
-	//
-	// let chain_name = chain_spec.name().to_string();
-	// let genesis_hash = client.block_hash(0).ok().flatten().expect("Genesis block exists; qed");
-	// let properties = chain_spec.properties();
-	// module.merge(ChainSpec::new(chain_name, genesis_hash, properties).into_rpc())?;
+    // You probably want to enable the `rpc v2 chainSpec` API as well
+    //
+    // let chain_name = chain_spec.name().to_string();
+    // let genesis_hash = client.block_hash(0).ok().flatten().expect("Genesis block exists; qed");
+    // let properties = chain_spec.properties();
+    // module.merge(ChainSpec::new(chain_name, genesis_hash, properties).into_rpc())?;
 
-	Ok(module)
+    Ok(module)
 }
